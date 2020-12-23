b72696d2e0fc38f9c0f70d342759c04b88d61db8
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-12-22 23:17:48 -0500

    File upload

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -52,7 +52,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, toke: token, status: 'You are Sucessfully logged in' });
+  res.json({ success: true, token: token, status: 'You are Sucessfully logged in' });
 });
 
 router.get('/logout', (req, res) => {
